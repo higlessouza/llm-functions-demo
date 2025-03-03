c28232f17ec9765646b8e4b8e716795b517ece22
Higles Souza <higlessouza@gmail.com>
2025-03-03 15:48:19 -0300

    removendo key
    incluindo driver remoto do chrome
    
    primeira versão funcionando local do scraping

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from src.services.trf6_scraping_service import Trf6ScrapingService
 
 app = FastAPI()
+
+trf6_scraping_service = Trf6ScrapingService()
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+
+@app.get("/processo/{processo}")
+async def get_processo(processo: str):
+    resultado = trf6_scraping_service.consultar_processo(processo)
+    return resultado
